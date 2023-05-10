fc5b289b36cfbda75f04d8d08da6e9081bcc07d3
marcogiammari <marcogiammari@gmail.com>
2023-05-10 15:22:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            toDoList: [
+                {
+                    text: "fare la spesa",
+                    done: true
+                },
+                {
+                    text: "pagare le bollette",
+                    done: false
+                },
+                {
+                    text: "comprare un nuovo mouse",
+                    done: false
+                }
+            ]
+        };
+    },
+    methods: {
+        isDone(i) {
+            return (this.toDoList[i].done) ? "done" : null
+        }
+    },
+}).mount("#app");
